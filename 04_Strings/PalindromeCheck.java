cd010874b878a816b95fb55f7eeefb91421b1124
Ayushmanraj523 <ayushmanraj925@gmail.com>
2026-01-14 22:43:38 +0530

    Solved PalindromeCheck problem

---
+++ 04_Strings/PalindromeCheck.java
@@ -0,0 +1,21 @@
+
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        String str = "madam";
+        boolean isPalindrome = true;
+        
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+        
+        System.out.println("Is '" + str + "' palindrome? " + isPalindrome);
+    }
+
+}
